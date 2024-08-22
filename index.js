c5c74ede3cf6f0fb1c5e338f2dea25b2d28a2ad9
HXPNOTIC0 <jimenezmateo798@gmail.com>
2024-08-22 04:02:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["TRABAJANDO PARA HXPNOTIC","WORKING FOR HXPNOTIC"];
 
 
 let currentIndex = 0;
